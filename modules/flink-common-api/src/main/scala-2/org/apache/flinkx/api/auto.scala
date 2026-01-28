4b87e3b88920753bbe5f40f13ce9fd1a72f5c3d5
adaroussin <arnaud.daroussin@datadome.co>
2026-01-29 00:38:28 +0100

    feat: Separate type-information derivation into auto and semiauto

---
+++ modules/flink-common-api/src/main/scala-2/org/apache/flinkx/api/auto.scala
@@ -0,0 +1,30 @@
+package org.apache.flinkx.api
+
+/** Provides automatic derivation of TypeInformation for Scala types.
+  *
+  * Import this object to enable automatic type-information derivation for ADT (case classes, sealed traits) and have
+  * access to implicitly available type-information for Scala types, collections, Java types, etc.
+  *
+  * ==Usage==
+  *
+  * ===Automatic Derivation===
+  *
+  * Simply import `org.apache.flinkx.api.auto._` to enable automatic TypeInformation resolution:
+  *
+  * {{{
+  * import org.apache.flinkx.api.auto._
+  *
+  * case class User(id: String, age: Int)
+  *
+  * // TypeInformation is automatically derived
+  * val env = StreamExecutionEnvironment.getExecutionEnvironment
+  * env.fromElements(User("alice", 30), User("bob", 25))
+  * }}}
+  *
+  * @see
+  *   [[semiauto]] for explicit/manual derivation
+  * @see
+  *   [[LowPrioImplicits.deriveTypeInformation]] for the automatic derivation method
+  */
+// Implicits priority order (linearization): auto > HighPrioImplicits > LowPrioImplicits
+object auto extends LowPrioImplicits with HighPrioImplicits
